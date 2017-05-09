da53500782078624b36cdd9b538f18e78a745940
Hannu Kamarainen <hannu.kamarainen@csc.fi>
2017-05-09 08:50:50 +0300

    Add proper nginx and gunicorn config files

--- ansible/roles/deploy/vars/gunicorn_conf.py
+++ ansible/roles/deploy/vars/gunicorn_conf.py
@@ -1,7 +1,23 @@
-bind = "0.0.0.0:8000"
-workers = 3
+"""
+gunicorn WSGI server configuration.
+
+to run:
+gunicorn -b localhost:8000 metax_api.wsgi:application
+
+"""
+from multiprocessing import cpu_count
+from os import environ
+
+def max_workers():
+    return cpu_count() * 2 + 1
+
+bind = '0.0.0.0:8000'
+max_requests = 1000
+worker_class = 'gevent'
+workers = max_workers()
+
 secure_scheme_headers = {
- 'X-FORWARDED-PROTOCOL': 'ssl',
- 'X-FORWARDED-PROTO': 'https',
- 'X-FORWARDED-SSL': 'on'
- }
+    'X-FORWARDED-PROTOCOL': 'ssl',
+    'X-FORWARDED-PROTO': 'https',
+    'X-FORWARDED-SSL': 'on'
+}
