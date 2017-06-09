f6d5a8e9492bd82e378ec20a980729a04c044031
Juha-Matti Lehtinen <junnu.lehtinen@gmail.com>
2017-06-09 15:58:32 +0300

    Change gunicorn service to use unix socket. In local dev env, gunicorn is not installed as service but run locally. Skip rabbitmq tasks if already installed. Add handler for rebooting for future. Django tasks now run  as django_user. Parameterize nginx proxy pass depending on whether gunicorn is used as service using socket or as locally run tcp connection

--- ansible/roles/gunicorn/templates/gunicorn_conf.py
+++ ansible/roles/gunicorn/templates/gunicorn_conf.py
@@ -1,9 +1,6 @@
 """
 gunicorn WSGI server configuration.
 
-to run:
-gunicorn -b localhost:8000 metax_api.wsgi:application
-
 """
 from multiprocessing import cpu_count
 from os import environ
@@ -11,7 +8,6 @@ from os import environ
 def max_workers():
     return cpu_count() * 2 + 1
 
-bind = '0.0.0.0:8000'
 max_requests = 1000
 worker_class = 'gevent'
 workers = max_workers()
