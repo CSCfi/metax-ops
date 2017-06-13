5c5ae19a0c600c9e6993adf57b5cb1e6fac2e499
Juha-Matti Lehtinen <junnu.lehtinen@gmail.com>
2017-06-13 10:46:16 +0300

    CSCMETAX-61: [Refactor] Modify backup tasks to use another environment variable.

--- ansible/roles/backup/templates/metax_backup.sh
+++ ansible/roles/backup/templates/metax_backup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 TIMESTAMP=$(date +%Y%m%d%H%M%S)
-pg_dump -f {{ metax_db_backup_path }}/archive/metax_db_{{ deployment_environment_id }}_backup_${TIMESTAMP}.dump
+pg_dump $METAX_DATABASE_NAME -f {{ metax_db_backup_archive_path }}/metax_db_{{ deployment_environment_id }}_backup_${TIMESTAMP}.dump
--- ansible/roles/backup/templates/metax_backup_sync.sh
+++ ansible/roles/backup/templates/metax_backup_sync.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-nextcloudcmd -u {{ metax_backup_username }} -p '{{ metax_backup_password }}' {{ metax_db_backup_path }}/archive https://kannu.csc.fi/remote.php/webdav/METAX_DB_BACKUP
+nextcloudcmd -u {{ metax_backup_username }} -p '{{ metax_backup_password }}' {{ metax_db_backup_archive_path }} https://kannu.csc.fi/remote.php/webdav/METAX_DB_BACKUP
