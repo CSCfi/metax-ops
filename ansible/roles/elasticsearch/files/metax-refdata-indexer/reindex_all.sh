2b00480b91731d9cae2cb20f85e575b50f2ee2b7
Juha-Matti Lehtinen <junnu.lehtinen@gmail.com>
2017-08-01 12:31:31 +0300

    CSCMETAX-117: [ADD] Index reference data on dataserver provisioning

--- ansible/roles/elasticsearch/files/metax-refdata-indexer/reindex_all.sh
+++ ansible/roles/elasticsearch/files/metax-refdata-indexer/reindex_all.sh
@@ -2,6 +2,11 @@
 # Script to reindex all reference data and organization data.
 # Run only from the folder where es_index_data.py exists.
 
+if [ "$USER" != "metax-user" ]; then
+    echo "Run this as metax-user"
+    exit 1
+fi
+
 source /srv/metax/pyenv/bin/activate
 cd /srv/refdata_indexer
 python es_index_data.py remove_and_recreate_index=all types_to_reindex=all
