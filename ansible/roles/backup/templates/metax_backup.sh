4025c6929cb05a3b824bb2a1cff769e4272a301e
Juha-Matti Lehtinen <junnu.lehtinen@gmail.com>
2018-06-06 20:05:35 +0300

    CSCMETAX-61: [REF]
    - Move some tasks around to more suitable roles
    - Remove waste
    - Create database and backup directories as required in res-paas
    - Update wal archiving command as required in res-paas

--- ansible/roles/backup/templates/metax_backup.sh
+++ ansible/roles/backup/templates/metax_backup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 TIMESTAMP=$(date +%Y%m%d%H%M%S)
-pg_basebackup -F t -D {{ metax_db_backup_archive_path }}/metax_db_{{ deployment_environment_id }}_backup_${TIMESTAMP}
+pg_basebackup -F t -D {{ metax_db_backup_path }}/metax_db_{{ deployment_environment_id }}_backup_${TIMESTAMP}
