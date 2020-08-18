2d54390aa1a3763c275ce265b9b2f4f04d7f57a5
Tommi Pulli <tommi.pulli7@gmail.com>
2020-08-18 15:32:26 +0300

    CSCFAIRMETA-182: [FIX] ansible stuff and elasticsearch service
    - Fixed bugs in writer role when commiting changes
    - Fixed provision script for refdata
    - Removed elasticsearch operation status check because return
      values was changing for each api and status logs were not
      informative

--- ansible/roles/refdata/files/metax-refdata-indexer/service/elasticsearch_service.py
+++ ansible/roles/refdata/files/metax-refdata-indexer/service/elasticsearch_service.py
@@ -33,11 +33,11 @@ class ElasticSearchService:
 
     def create_index(self, index, filename):
         _logger.info("Trying to create index " + index)
-        self._operation_ok(self.es.indices.create(index=index, body=self._get_json_file_as_str(filename)))
+        self.es.indices.create(index=index, body=self._get_json_file_as_str(filename))
 
     def delete_index(self, index):
         _logger.info("Trying to delete index " + index)
-        self._operation_ok(self.es.indices.delete(index=index, ignore=[404]))
+        self.es.indices.delete(index=index, ignore=[404])
 
     def delete_and_update_indexable_data(self, index, doc_type, indexable_data_list):
         if len(indexable_data_list) > 0:
@@ -50,19 +50,14 @@ class ElasticSearchService:
             self._delete_all_documents_from_index_with_type(index, doc_type)
             _logger.info("Trying to bulk update reference data with type " + doc_type + " to index " + index)
 
-            self._operation_ok(self.es.bulk(body=bulk_update_str, request_timeout=30))
+            self.es.bulk(body=bulk_update_str, request_timeout=30)
 
         else:
             _logger.info("No data for {}".format(doc_type))
 
     def _delete_all_documents_from_index_with_type(self, index, doc_type):
         _logger.info("Trying to delete all documents from index " + index + " having type " + doc_type)
-        return self._operation_ok(
-            self.es.delete_by_query(
-                index=index,
-                request_timeout=30,
-                body="{\"query\": { \"match\": {\"type\": \"%s\"}}}" % doc_type)
-        )
+        self.es.delete_by_query(index=index, body="{\"query\": { \"match\": {\"type\": \"%s\"}}}" % doc_type)
 
     def _create_bulk_update_row_for_indexable_data(self, index, indexable_data_item):
         return "{\"index\":{\"_index\": \"" + index + "\"}}\n" + indexable_data_item
@@ -71,13 +66,6 @@ class ElasticSearchService:
         return "{\"delete\":{\"_index\": \"" + \
             index + "\", \"_id\":\"" + \
             indexable_data_item.get_es_document_id() + "\"}}"
-
-    def _operation_ok(self, op_response):
-        # bulk ops return errors and others acknowledged
-        if op_response.get('errors') is False or op_response.get('acknowledged') is True:
-            _logger.info('Success')
-        else:
-            _logger.info('Elasticsearch operation failed')
 
     def _get_json_file_as_str(self, filename):
         with open(filename) as json_data:
